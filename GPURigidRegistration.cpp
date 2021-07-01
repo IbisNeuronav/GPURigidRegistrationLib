a0bd9788c429018a513703a755f869f38e2b4403
Houssem <hgueziri@users.noreply.github.com>
2021-07-01 19:19:49 -0400

    Modification to the main function (#2)
    * Style: naming ImageType instead of IbisItkFloat3ImageType
    
    * Add initial parameters
    
    * Write output transform in a file
    
    * read moving and fixed images in different image types

--- GPURigidRegistration.cpp
+++ GPURigidRegistration.cpp
@@ -1,62 +1,130 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
+#include "itkEuler3DTransform.h"
+#include "itkTransformFileWriter.h"
 #include "gpu_rigidregistration.h"
 #include "vtkTransform.h"
+
 
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc < 4)
     {
         // check existing file
         std::cerr << "Usage: " << std::endl;
         std::cerr << argv[0];
-        std::cerr << " <InputFileName>";
+        std::cerr << " <MovingImage> <FixedImage>";
+        std::cerr << " <InitialTransform> <OutputTransform>";
         std::cerr << std::endl;
+
+        //TODO: add parameter description 
+
+        return EXIT_FAILURE;
+    }
+    
+    using ImageType = itk::Image<float, 3>;
+    using MovingImageReaderType = itk::ImageFileReader<ImageType>;
+    using FixedImageReaderType = itk::ImageFileReader<ImageType>;
+
+    // Reading the first file
+    MovingImageReaderType::Pointer movingReader = MovingImageReaderType::New();
+    movingReader->SetFileName(argv[1]);
+    std::cout << "Reading Moving Image... " << argv[1] << std::endl;
+    try
+    {
+        movingReader->Update();
+    }
+    catch( itk::ExceptionObject & error )
+    {
+        std::cerr << "Error: " << error << std::endl;
         return EXIT_FAILURE;
     }
 
-  // Reading the first file
-    typedef itk::Image<float, 3> IbisItkFloat3ImageType;
-    using ReaderType = itk::ImageFileReader<IbisItkFloat3ImageType>;
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName(argv[1]);
-    reader->Update();
+    ImageType::Pointer movingImage = movingReader->GetOutput();
 
-    IbisItkFloat3ImageType::Pointer image = reader->GetOutput();
+    //Reading the second file 
+    FixedImageReaderType::Pointer fixedReader = FixedImageReaderType::New();
+    fixedReader->SetFileName(argv[2]);
+    std::cout << "Reading Fixed Image... " << argv[2] << std::endl;
+    try
+    {
+        fixedReader->Update();
+    }
+    catch( itk::ExceptionObject & error )
+    {
+        std::cerr << "Error: " << error << std::endl;
+        return EXIT_FAILURE;
+    }
 
-  //Reading the second file 
-    using ReaderType = itk::ImageFileReader<IbisItkFloat3ImageType>;
-    ReaderType::Pointer reader2 = ReaderType::New();
-    reader->SetFileName(argv[2]);
-    reader->Update();
+    ImageType::Pointer fixedImage = fixedReader->GetOutput();
 
-    IbisItkFloat3ImageType::Pointer image2 = reader->GetOutput();
-
-    return EXIT_SUCCESS;
+    //TODO: read initialization transform in a vtkTransform
 
     GPU_RigidRegistration* rigidRegistrator = new GPU_RigidRegistration();
 
     // Initialize parameters
-    /*m_rigidRegistrator->SetNumberOfPixels(ui->numebrOfPixelsDial->value());
-    m_rigidRegistrator->SetOrientationSelectivity(ui->selectivityDial->value());
-    m_rigidRegistrator->SetPopulationSize(ui->populationSizeDial->value());
-    m_rigidRegistrator->SetPercentile(ui->percentileComboBox->itemData(ui->percentileComboBox->currentIndex()).toDouble());
-    m_rigidRegistrator->SetUseMask(ui->computeMaskCheckBox->isChecked());
-    m_rigidRegistrator->SetDebug(debug, &debugStringStream);*/
+    rigidRegistrator->SetNumberOfPixels(128000);
+    rigidRegistrator->SetOrientationSelectivity(32);
+    rigidRegistrator->SetPopulationSize(100);
+    rigidRegistrator->SetPercentile(0.8);
+    rigidRegistrator->SetUseMask(true);
 
     // Set image inputs
-    rigidRegistrator->SetItkSourceImage(image);
-    rigidRegistrator->SetItkTargetImage(image2);
+    rigidRegistrator->SetItkSourceImage(movingImage);
+    rigidRegistrator->SetItkTargetImage(fixedImage);
 
     // Set transform inputs
-    vtkTransform * t = vtkTransform::New();
-    rigidRegistrator->SetVtkTransform(t);
+    vtkTransform * transform = vtkTransform::New();
+    rigidRegistrator->SetVtkTransform(transform);
 
     rigidRegistrator->runRegistration();
 
-    delete rigidRegistrator;
-    t->Delete();
+    //TODO: write output transform in argv[4]
+    using ScalarType = double;
+    using ItkRigidTransformType = itk::Euler3DTransform<ScalarType>;
+    ItkRigidTransformType::Pointer outputTransform = ItkRigidTransformType::New();
+    ItkRigidTransformType::MatrixType matrix;
+    ItkRigidTransformType::OffsetType offset;
+
+    for( unsigned int i = 0; i < 3; i++ )
+    {
+        for( unsigned int j = 0; j < 3; j++ )
+        {
+            matrix.GetVnlMatrix().set(i, j, transform->GetMatrix()->GetElement(i, j));
+        }
+        offset[i] = transform->GetMatrix()->GetElement(i, 3);
+    }
+
+    outputTransform->SetMatrix(matrix);
+    outputTransform->SetOffset(offset);
+    
+    // temporary testing
+    for( size_t i = 0; i < 4; i++ )
+    {
+        for( size_t j = 0; j < 4; j++ )
+        {
+            std::cout << transform->GetMatrix()->GetElement(i, j) << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Write transform in " << argv[4] << std::endl;
+    using TransformWriterType = itk::TransformFileWriterTemplate<ScalarType>;
+    TransformWriterType::Pointer writer = TransformWriterType::New();
+    writer->SetInput(outputTransform);
+    writer->SetFileName(argv[4]);
+    try
+    {
+        writer->Update();
+    }
+    catch( itk::ExceptionObject & error )
+    {
+        std::cerr << "Error while saving the transforms:" << error << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
